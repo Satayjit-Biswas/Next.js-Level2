08e18887232a5a1aaef0dcbec9ec74e990afd037
Satayjit-Biswas <satayjitbiswas2@gmail.com>
2023-09-02 21:43:39 +0600

    Data Fetch and Add 404 Page

---
+++ project3 (Data Fatching With App Router)/src/app/not-found.js
@@ -0,0 +1,10 @@
+
+const NotfoundPage = () => {
+  return (
+    <div>
+      <h2>404 No Page</h2>
+    </div>
+  );
+};
+
+export default NotfoundPage;
--- project3 (Data Fatching With App Router)/src/app/page.js
+++ project3 (Data Fatching With App Router)/src/app/page.js
@@ -1,10 +1,21 @@
-const HomePage = () => {
-  return (
-    <div>
-      <h1 className="text-4xl text-center my-10">Welcome To Watch Gallery</h1>
-      <div className="col-span-9 grid grid-cols-3 gap-5 p-10 w-[80%] mx-auto"></div>
-    </div>
-  );
+import Products from "@/components/UI/Products";
+import { getData } from "@/components/utils/getData";
+
+const HomePage = async () => {
+    const data = await getData() 
+    // console.log(data);
+    return (
+        <div>
+            <h1 className="text-4xl text-center my-10">
+                Welcome To Watch Gallery
+            </h1>
+            <div className="col-span-9 grid grid-cols-3 gap-5 p-10 w-[80%] mx-auto">
+                {data.map((product) => (
+                    <Products key={product.id} product={product}></Products>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default HomePage;
